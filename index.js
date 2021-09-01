cfcb76b1ba1689788b4d6699159e6a14b5a74a43
Gabriel Secario Teixeira <gabrielsecario@gmail.com>
2021-09-01 19:03:32 -0300

    Corrigindo texto de exp prof

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function mudarconteudo(param){
         document.getElementById("txt1").innerHTML = "Meu principal hobby/passatempo é jogar, principalmente jogos online e competitivos. <br> Além de jogar, gosto muito de escutar música, assistir séries/lives e ler (embora não tenha lido muito há algum tempo)"
         document.getElementById("titulo").innerHTML = "Hobbies"
     } else if(parametro == "exp"){
-        document.getElementById("txt1").innerHTML = "Trabalhei como atendente de telemarketing por pouco mais de um ano, atendendo tanto SAC quanto suporte técnico de banda larga. <br> Além dessa minha experiência registrada em carteira, já trabalhei como atendente voluntáriou <br> Trabalhei como monitor de Algoritmos por dois semestres na faculdade (Fatec Itaquaquecetuba) e, atualmente, trabalho como autônomo fazendo manutenção de computadores."
+        document.getElementById("txt1").innerHTML = "Trabalhei como atendente de telemarketing por pouco mais de um ano, atendendo tanto SAC quanto suporte técnico de banda larga. <br> Além dessa minha experiência registrada em carteira, já trabalhei como atendente voluntário <br> Trabalhei como monitor de Algoritmos por dois semestres na faculdade (Fatec Itaquaquecetuba) e, atualmente, trabalho como autônomo fazendo manutenção de computadores."
         document.getElementById("titulo").innerHTML = "Experiência Profissional"
     } else if(parametro == "formacao"){
         document.getElementById("txt1").innerHTML = "Ensino médio completo: E. E. Profº Elias Zugaib; <br> Curso de pacote Office: Escola Romafla de Informática <br> Curso de Hardware: Escola Romafla de Informática <br> Técnico em informática: Etec de Poá; <br> Tecnólogo em Gestão de Tecnologia da Informação (cursando o 5º de seis semestres) <br>"
