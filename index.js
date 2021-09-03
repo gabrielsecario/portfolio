71c3d5bb0b743ac039721cfe23e1d48a463a0a3e
Gabriel Secario Teixeira <gabrielsecario@gmail.com>
2021-09-03 17:42:32 -0300

    Atualizando margem do footer

--- index.js
+++ index.js
@@ -19,6 +19,4 @@ function mudarconteudo(param){
         document.getElementById("txt1").innerHTML = "Minha meta atual é conseguir me inserir no mercado de TI.<br> A princípio, conseguir um estágio na área de desenvolvimento e me tornar um DEV competente <br> Podendo, assim, investir ainda mais nos meus estudos e almejar metas ainda maiores."
         document.getElementById("titulo").innerHTML = "Metas"
     }
-if (document.querySelector('body').offsetHeight > window.innerHeight)
-document.querySelector('footer').classList.add('footer-relative');
 }
